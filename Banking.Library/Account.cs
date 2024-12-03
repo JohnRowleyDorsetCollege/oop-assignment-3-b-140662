b8ff35a5254c1b476778dff3ee37151e1c268a4a
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-12-03 11:07:10 +0000

    Added Account

---
+++ Banking.Library/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Library
+{
+    public abstract class Account
+    {
+        public string AccountName { get; set; }
+
+        public Account(string firstName, string lastName)
+        {
+            AccountName = GenerateAccountName(firstName, lastName);
+        }
+
+        public string GenerateAccountName(string firstName, string lastName)
+        {
+            // Introduce the rules for creating account names here
+            throw new NotImplementedException();
+        }
+    }
+}
