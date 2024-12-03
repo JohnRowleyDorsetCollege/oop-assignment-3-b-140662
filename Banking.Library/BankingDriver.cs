6d2e992e3718a4bc22286b032158eee73e97354f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-12-03 11:09:20 +0000

    Added Menu functions

--- Banking.Library/BankingDriver.cs
+++ Banking.Library/BankingDriver.cs
@@ -4,7 +4,37 @@
     {
         public static void Run()
         {
-            Console.WriteLine("Banking Application Has Started");
+            Console.WriteLine("Banking Application has started");
+
+            Login();
+        }
+
+        //This displays menu and handles interaction from user
+        // It should probably call either the Bank Menu or Customer Menu as required
+        // Then they should show sub menus as required
+        public static void Login()
+        {
+            Console.WriteLine("Login Menu");
+        }
+
+        public static void BankMenu()
+        {
+            Console.WriteLine("Bank  Menu");
+        }
+
+        public static void BankSubMenu()
+        {
+            Console.WriteLine("BankSubMenu");
+        }
+
+        public static void CustomerMenu()
+        {
+            Console.WriteLine("CustomerMenu");
+        }
+
+        public static void CustomerSubMenu()
+        {
+            Console.WriteLine("CustomerSubMenu");
         }
     }
 }
